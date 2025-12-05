835878ec0ff756a434b641156e2f18f15a6c5bca
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-12-05 18:44:29 +0000

    add implementation of Lagrange on triangle

---
+++ simplefem/lagrange.py
@@ -0,0 +1,43 @@
+"""Lagrange element on a triangle."""
+import numpy as np
+from simplefem.polynomials import tabulate
+
+class LagrangeElementTriangle:
+    """A Lagrange element on a triangle."""
+    def __init__(self, degree):
+        """Initialise."""
+        self.degree = degree
+        self.evaluation_points = np.array([
+            [j/degree, i/degree]
+            for i in range(degree + 1)
+            for j in range(degree + 1 - i)
+        ])
+
+        self.coeffs = np.linalg.inv(tabulate(self.evaluation_points, degree))
+
+    def tabulate(self, points: np.ndarray[float]) -> np.ndarray[float]:
+        """Tabulate the values of the basis functions at a set of points.
+
+        Args:
+            points: The points to evaluate at. This should be a two-dimensional array:
+                    the entry [i][j] of this should be the jth coordinate of the ith point
+
+        Returns:
+            The value of each basis function at each point. This function returns a
+            two-dimensional array: the entry [i][j] of this is the value of the ith
+            basis function at the jth point
+        """
+        return self.coeffs @ tabulate(points, self.degree)
+
+
+def lagrange_element(degree: int) -> LagrangeElementTriangle:
+    """Create a Lagrange element on a triangle.
+
+    Args:
+        degree: The polynomial degree
+
+    Returns:
+        A Lagrange element on a triangle
+    """
+    assert degree > 0
+    return LagrangeElementTriangle(degree)
---
+++ test/test_lagrange.py
@@ -0,0 +1,36 @@
+import numpy as np
+from simplefem import lagrange_element
+import pytest
+
+N = 4
+
+
+def test_lagrange1_element():
+    points = np.array([[i/N, j/N] for i in range(N + 1) for j in range(N + 1 - i)])
+
+    e = lagrange_element(1)
+    values = e.tabulate(points)
+
+    x = points[:, 0]
+    y = points[:, 1]
+
+    assert np.allclose(values[0], 1 - x - y)
+    assert np.allclose(values[1], x)
+    assert np.allclose(values[2], y)
+
+
+def test_lagrange2_element():
+    points = np.array([[i/N, j/N] for i in range(N + 1) for j in range(N + 1 - i)])
+
+    e = lagrange_element(2)
+    values = e.tabulate(points)
+
+    x = points[:, 0]
+    y = points[:, 1]
+
+    assert np.allclose(values[0], (1 - x - y) * (1 - 2 * x - 2 * y))
+    assert np.allclose(values[1], 4 * x * (1 - x - y))
+    assert np.allclose(values[2], x * (2 * x - 1))
+    assert np.allclose(values[3], 4 * y * (1 - x - y))
+    assert np.allclose(values[4], 4 * x * y)
+    assert np.allclose(values[5], y * (2 * y - 1))
